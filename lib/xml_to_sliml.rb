b50ae1c0a1345c7c71b393b03117f9ef5a3c9a61
Your Name <you@example.com>
2018-01-23 20:08:21 +0000

    1st commit

---
+++ lib/xml_to_sliml.rb
@@ -0,0 +1,62 @@
+#!/usr/bin/env ruby
+
+# file: xml_to_sliml.rb
+
+require 'rexle'
+
+
+class XmlToSliml
+
+  attr_reader :to_s
+
+  def initialize(obj)
+
+    @to_s = ''
+    doc = (obj.is_a?(Rexle) or obj.is_a?(Rexle::Element)) ? obj : Rexle.new(obj)
+    @to_s << scanbuild(doc.to_a)    
+
+  end
+
+  private
+
+  # format attributes
+  #
+  def fa(h)
+
+    "{%s}" % h.map {|k, v| "%s: %s" % [k, v.inspect]}.join(', ')
+
+  end
+
+  def scanbuild(x, indent=0)
+
+    name, attributes, *remaining = x
+
+    children = remaining.shift
+    text = ''
+
+      
+    if children.is_a? Array then
+      nested = scanbuild(children, indent+1) 
+    elsif children
+      text = children
+    end
+
+    pad = '  ' * indent
+
+    buffer = pad + name
+    buffer << ' ' + fa(attributes) if attributes.any?
+    buffer << ' ' + text unless text.strip.empty?
+    
+
+    while remaining.any? do
+      children = remaining.shift
+      if children and children.is_a? Array then
+        buffer << "\n" + scanbuild(children, indent+1) 
+      end
+    end
+
+    buffer
+  end
+
+ 
+end
---
+++ xml_to_sliml.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name = 'xml_to_sliml'
+  s.version = '0.1.0'
+  s.summary = 'Transforms XML into Sliml.'
+  s.authors = ['James Robertson']
+  s.files = Dir['lib/xml_to_sliml.rb']
+  s.add_runtime_dependency('rexle', '~> 1.4', '>=1.4.12') 
+  s.signing_key = '../privatekeys/xml_to_sliml.pem'
+  s.cert_chain  = ['gem-public_cert.pem']
+  s.license = 'MIT'
+  s.email = 'james@jamesrobertson.eu'
+  s.homepage = 'https://github.com/jrobertson/xml_to_sliml'
+end
